eb68ef19da79b1a8c62e7c97ae8a3ccd0e97870a
Yang Wang <gnavvy@uber.com>
2017-10-19 14:55:11 -0700

    make y-flipping optional

--- src/web-mercator-utils.js
+++ src/web-mercator-utils.js
@@ -215,7 +215,8 @@ export function makeViewMatricesFromMercatorParams({
   bearing,
   altitude,
   distanceScales,
-  meterOffset = null
+  meterOffset = null,
+  flipY = true
 }) {
   const center = getMercatorWorldPosition({longitude, latitude, zoom, distanceScales, meterOffset});
 
@@ -234,7 +235,11 @@ export function makeViewMatricesFromMercatorParams({
   });
 
   const vm = createMat4();
-  mat4_scale(vm, viewMatrixUncentered, [1, -1, 1]);
+
+  if (flipY) {
+    mat4_scale(vm, viewMatrixUncentered, [1, -1, 1]);
+  }
+
   const viewMatrixCentered = mat4_translate(vm, vm, new Vector3(center).negate());
 
   return {
