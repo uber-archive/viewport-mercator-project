73ac98f80ccd5563fa473c22bf4789b2e7eb6f6f
Ib Green <ibgreen@users.noreply.github.com>
2017-11-15 11:22:48 -0800

    Restore explicit exports (#43)

--- src/index.js
+++ src/index.js
@@ -4,5 +4,17 @@ export {default as WebMercatorViewport} from './web-mercator-viewport';
 // Legacy class name
 export {default as PerspectiveMercatorViewport} from './web-mercator-viewport';
 
-export * from './web-mercator-utils';
 export {fitBounds} from './web-mercator-viewport';
+
+export {
+  projectFlat,
+  unprojectFlat,
+  getMercatorMeterZoom,
+  getMercatorDistanceScales,
+  getMercatorWorldPosition,
+  makeViewMatricesFromMercatorParams,
+  makeUncenteredViewMatrixFromMercatorParams,
+  makeProjectionMatrixFromMercatorParams,
+  getFov,
+  getClippingPlanes
+} from './web-mercator-utils';
