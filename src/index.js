36a9671f480839d4c938845e1c691f4db53b5cf7
Xiaoji Chen <xiaoji@uber.com>
2018-01-08 21:21:01 -0800

    move fitBounds util to separate file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ export {default as WebMercatorViewport} from './web-mercator-viewport';
 // Legacy class name
 export {default as PerspectiveMercatorViewport} from './web-mercator-viewport';
 
-export {fitBounds} from './web-mercator-viewport';
+export {default as fitBounds} from './fit-bounds';
 export {default as normalizeViewportProps} from './normalize-viewport-props';
 export {default as flyToViewport} from './fly-to-viewport';
 
