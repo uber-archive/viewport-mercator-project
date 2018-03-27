265ce8f29f4e8649a8aca37d845137b8485a2062
Xiaoji Chen <xiaoji@uber.com>
2018-03-27 12:00:29 -0700

    fix getMeterZoom

--- src/web-mercator-utils.js
+++ src/web-mercator-utils.js
@@ -68,11 +68,12 @@ export function worldToLngLat([x, y], scale) {
 }
 
 // Returns the zoom level that gives a 1 meter pixel at a certain latitude
-// S=C*cos(y)/2^(z+8)
+// S=C*cos(y)/2^z/TILE_SIZE
+// S=C*cos(y)/2^(z+9)
 export function getMeterZoom({latitude}) {
   assert(Number.isFinite(latitude));
   const latCosine = Math.cos(latitude * DEGREES_TO_RADIANS);
-  return scaleToZoom(EARTH_CIRCUMFERENCE * latCosine) - 8;
+  return scaleToZoom(EARTH_CIRCUMFERENCE * latCosine) - 9;
 }
 
 /**
